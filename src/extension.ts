a694ddbb199d1f68f92d5e833608f3fab5a793a0
futantan <luckytantanfu@gmail.com>
2019-06-27 21:23:47 +0800

    change cmd

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,14 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World!');
-	});
+  let disposable = vscode.commands.registerCommand(
+    "extension.githubZen",
+    () => {
+      vscode.window.showInformationMessage("zen");
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
